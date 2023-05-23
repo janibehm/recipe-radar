403a0a25d4a75256175421d0c128f541bf3c967a
janibehm <jani.behm@gmail.com>
2023-05-23 01:23:48 +0000

    fixing merge issues

--- pages/recipes.js
+++ pages/recipes.js
@@ -1,156 +1,45 @@
-import Link from 'next/link';
-import { useState } from 'react';
-import styles from '../styles/recipe.module.css';
-import axios from 'axios';
+import React from 'react';
+import Navbar from '../components/Navbar';
+import styles from '../styles/index.module.css';
+import useFetchData from '../hooks/fetchData';
+import Card from '../components/Card';
 
-const countries = [
-  'Country 1',
-  'Country 2',
-  'Country 3',
-  // Add more countries to the list
-];
+const Recipes = () => {
+  const { data, loading, error } = useFetchData('http://localhost:4000/recipes');
+  console.log('Recipes retrieved:', data);
 
-const RecipePage = () => {
-  const [name, setName] = useState('');
-  const [author, setAuthor] = useState('');
-  const [country, setCountry] = useState('');
-  const [quantity, setQuantity] = useState('');
-  const [ingredient, setIngredient] = useState('');
-  const [instructions, setInstructions] = useState('');
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-  };
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
-  const handleAuthorChange = (event) => {
-    setAuthor(event.target.value);
-  };
-
-  const handleCountryChange = (event) => {
-    setCountry(event.target.value);
-  };
-
-  const handleQuantityChange = (event) => {
-    setQuantity(event.target.value);
-  };
-
-  const handleIngredientChange = (event) => {
-    setIngredient(event.target.value);
-  };
-
-  const handleInstructionsChange = (event) => {
-    setInstructions(event.target.value);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    console.log('Form submitted:', {
-      name,
-      author,
-      country,
-      quantity,
-      ingredient,
-      instructions,
-    });
-
-    const payload = {
-      name,
-      author,
-      country,
-      quantity,
-      ingredient,
-      instructions,
-    };
-
-    try {
-      const response = await axios.post('http://localhost:4000/recipes', payload);
-      console.log('Recipe posted:', response.data);
-    } catch (error) {
-      console.error('Failed to post recipe:', error);
-    }
-  };
   return (
-    <div className={styles.container}>
-      <h1>Recipe Page</h1>
-      <Link href="/">Go to Home Page</Link>
-   
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="name">Name:</label>
-            <input required
-              type="text"
-              id="name"
-              value={name}
-              onChange={handleNameChange}
-            />
-          </div>
+    <div>
+      <Navbar />
+      <div className={styles.container}>
+        <h1>Our recipes</h1>
+         <div className={styles.imageContainer}>
+         {data.map((recipe) => (
+          <Card
+            key={recipe.id}
+            name={recipe.name}
+            author={recipe.author}
+            country={recipe.country}
+            quantity={recipe.quantity}
+            ingredient={recipe.ingredient}
+            instructions={recipe.instructions}
+            imageUrl={recipe.imageUrl} 
+            alt={recipe.name} 
+          />
+        ))}
 
-          <div>
-            <label htmlFor="author">Author:</label>
-            <input required
-              type="text"
-              id="author"
-              value={author}
-              onChange={handleAuthorChange}
-            />
-          </div>
-
-          <div>
-            <label htmlFor="country">Recipe is from:</label>
-            <select required
-              id="country"
-              value={country}
-              onChange={handleCountryChange}
-            >
-              <option value="">Select a country</option>
-              {countries.map((country) => (
-                <option value={country} key={country}>
-                  {country}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <div>
-            <label htmlFor="quantity">Quantity:</label>
-            <input required
-              type="text"
-              id="quantity"
-              value={quantity}
-              onChange={handleQuantityChange}
-            />
-          </div>
-
-          <div>
-            <label htmlFor="ingredient">Ingredient:</label>
-            <input required
-              type="text"
-              id="ingredient"
-              value={ingredient}
-              onChange={handleIngredientChange}
-            />
-          </div>
-
-          <div>
-            <button type="button">Add more</button>
-          </div>
-
-          <div>
-            <label htmlFor="instructions">Instructions:</label>
-            <textarea
-              id="instructions"
-              value={instructions}
-              onChange={handleInstructionsChange}
-            />
-          </div>
-
-          <div>
-            <button type="submit">Post recipe</button>
-</div>
-</form>
-</div>
-
-);
+        </div> 
+      </div>
+    </div>
+  );
 };
 
-export default RecipePage;
+export default Recipes;
