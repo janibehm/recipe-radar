09ba6716489603f504704f69e89ed1543bd72fe4
janibehm <jani.behm@gmail.com>
2023-05-26 04:31:19 +0000

    added hero banner video

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,38 @@
-import Link from 'next/link';
-
-import styles from '../styles/index.module.css';
+import React from 'react';
+import Link from 'next/link';  // Import the Link component
 import Navbar from '../components/Navbar';
+import styles from '../styles/index.module.css';
 
 const Index = () => {
-
   return (
     <div>
-    <Navbar />
-    <div className={styles.container}>
-      <h1>Home Page</h1>
-      <Link href="/recipes">Go to Recipe Page</Link>   
-    </div>
+      <Navbar />
+      <div className={styles.container}>
+        <div className={styles.heroBanner}>
+       
+        <video autoPlay loop muted className={styles.heroVideo}>
+         <source src="/videos/food.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+      </video>
+        </div>
+        <h1>Looking for a recipes?</h1>
+
+        {/* Cards */}
+        <div className={styles.cardContainer}>
+          <Link href="/recipes">
+            <div className={styles.card}>Browse recipes</div>
+          </Link>
+          <Link href="/addRecipe">
+            <div className={styles.card}>Add recipes</div>
+          </Link>
+          <Link href="/projects">
+            <div className={styles.card}>Want to know more about our projects?</div>
+          </Link>
+          
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Index;
-
