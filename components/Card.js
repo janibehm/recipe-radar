c1fd1e7ccdd8c7b50dabc246998e1bb27f061671
janibehm <jani.behm@gmail.com>
2023-05-26 04:05:46 +0000

    when clicking the card expand the card

--- components/Card.js
+++ components/Card.js
@@ -1,27 +1,52 @@
 import React from 'react';
 import styles from '../styles/card.module.css';
 import Image from 'next/image';
+import { useState } from 'react';
 
 const Card = ({ name, author, country, instructions, imageUrl, alt, ingredients }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <div className={styles.card}>
-      <Image src={imageUrl} alt={alt} height={300} width={300}/>
-      <div className={styles.cardContent}>
-        <h2>{name}</h2>
-        <p>Author: {author}</p>
-        <p>Country: {country}</p>
-        <h3>Ingredients:</h3>
-        <ul>
-          {ingredients.map((ingredient, index) => (
-            <li key={index}>{`${ingredient.quantity} ${ingredient.ingredient}`}</li>
-          ))}
-        </ul>
-        <p>Instructions: {instructions}</p>
+    <div
+      className={`${styles.cardContainer} ${expanded ? styles.fullscreen : ''}`}
+      onClick={toggleExpanded}
+    >
+      <div className={styles.card}>
+        <div className={styles.cardHeader}>
+          <div className={styles.cardImage}>
+            <Image src={imageUrl} alt={alt} width={50} height={50} />
+          </div>
+          <h2>{name}</h2>
+        </div>
+        <button 
+          onClick={(e) => {
+            e.stopPropagation();
+            toggleExpanded();
+          }}
+        >
+          {expanded ? 'See less' : 'See more'}
+        </button>
+
+        {expanded && 
+          <div>
+            <p>Author: {author}</p>
+            <p>Country: {country}</p>
+            <p>Instructions: {instructions}</p>
+            <div>
+              <h3>Ingredients:</h3>
+              {ingredients.map((ingredient, index) => (
+                <p key={index}>{ingredient.quantity} of {ingredient.ingredient}</p>
+              ))}
+            </div>
+          </div>
+        }
       </div>
     </div>
   );
 };
 
-
 export default Card;
-
--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['media.istockphoto.com',"img.freepik.com","images.unsplash.com","th.bing.com","images.pexels.com"]
+    domains: ['media.istockphoto.com',"img.freepik.com","images.unsplash.com","th.bing.com","images.pexels.com","https://images.pexels.com/photos/2619967/pexels-photo-2619967.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"],
   },
 };
 
