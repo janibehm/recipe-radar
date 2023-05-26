6804e4b3dd25e2d2aa342cf260f4f27fa82e3583
janibehm <jani.behm@gmail.com>
2023-05-26 04:11:00 +0000

    search working

--- pages/recipes.js
+++ pages/recipes.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react'; 
 import Navbar from '../components/Navbar';
 import styles from '../styles/index.module.css';
 import useFetchData from '../hooks/fetchData';
 import Card from '../components/Card';
 
 const Recipes = () => {
+  const [search, setSearch] = useState(""); 
   const { data, loading, error } = useFetchData('http://localhost:4000/recipes');
-  console.log('Recipes retrieved:', data);
+
+  const filteredData = data.filter(recipe =>
+    recipe.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   if (loading) {
     return <div>Loading...</div>;
@@ -21,8 +25,14 @@ const Recipes = () => {
       <Navbar />
       <div className={styles.container}>
         <h1>Our recipes</h1>
+        <input
+          type="text"
+          placeholder="Search recipes..."
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
         <div className={styles.imageContainer}>
-          {data.map((recipe) => (
+          {filteredData.map((recipe) => (
             <Card
               key={recipe.id}
               name={recipe.name}
